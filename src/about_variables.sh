173c63dd4ca6a621a8374e633c10a4bb20a5d1c4
Marcin Bunsch <marcin@futuresimple.com>
2012-03-13 10:24:16 +0100

    MUCH better code

--- src/about_variables.sh
+++ src/about_variables.sh
@@ -1,13 +1,38 @@
 # About environment variables
+green "Variables"
 
-description "Set the variable to the right value" "about_variables.sh:7"
+test_setting_the_variable() {
+  variable=1
+  assertEqual 1 $variable
+}
 
-variable=__
+test_using_double_quotes() {
+  variable=2
+  assertEqual "foo 2" "foo $variable"
+}
 
-assertEqual 1 $variable
+test_unsetting_variables() {
+  local newVariable="Foooo"
+  unset newVariable
+  assertEqual $newVariable ""
+}
 
-description "Variables can be used in double quotes" "about_variables.sh:9"
+# description "Set the variable to the right value" "about_variables.sh:$LINENO"
 
-variable=__
+# variable=1
 
-assertEqual "foo 1" "foo $variable"
+# assertEqual 1 $variable
+
+# description "Variables can be used in double quotes" "about_variables.sh:$LINENO"
+
+# variable=1
+
+# assertEqual "foo 1" "foo $variable"
+
+# description "Variables can removed using unset" "about_variables.sh:$LINENO"
+
+# newVariable="Foooo"
+# unset newVariable
+
+# assertEqual $newVariable __
+
---
+++ support/functions.sh
@@ -0,0 +1,33 @@
+# http://stackoverflow.com/questions/7650438/bash-funcname-value-expanding
+# Required for the alias trick to work
+shopt -s expand_aliases
+
+green() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+red() {
+  echo -e "\033[31m$1\033[0m"
+}
+
+assertEqual() {
+  if [[ "$3" = "$4" ]]; then
+    green "  $1"
+  else
+    echo ''
+    red "  $1 has damaged your karma."
+    echo ''
+    echo "You have not yet reached enlightenment ..."
+    red  "  Expected '$3', got '$4'"
+    echo ''
+    echo "Please meditate on the following code:"
+    local filename=`grep  $1 src/* -l`
+    red  "  $filename:$2"
+    echo ''
+    echo "You are now 10/291 koans and 2/36 lessons away from reaching enlightenment"
+    # exit 1
+  fi
+}
+# This allows us to get the name of the functionw where this function was called
+# http://stackoverflow.com/questions/7650438/bash-funcname-value-expanding
+alias assertEqual='assertEqual ${FUNCNAME} ${LINENO}'
